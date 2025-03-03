0e7da68acf1afd2760139c42bef9aa14ea2e7742
Amar Mohammed <63112654+amarmohammed20@users.noreply.github.com>
2025-03-03 12:54:17 +0000

    Formatting

--- backend/Controllers/HadithController.cs
+++ backend/Controllers/HadithController.cs
@@ -3,44 +3,48 @@ using IlmMachine.Backend.Models;
 
 [Route("api/[controller]")]
 [ApiController]
-public class HadithController: ControllerBase
+public class HadithController : ControllerBase
 {
-    private static readonly string[] ArabicText = 
-    {
-        "حَدَّثَنَا عَبْدُ اللَّهِ...",
-        "رَوَى أَبُو هُرَيْرَةَ...",
-        "حَدَّثَنَا مُحَمَّدٌ..."
-    };
+	private static readonly string[] ArabicText =
+	{
+		"حَدَّثَنَا عَبْدُ اللَّهِ...",
+		"رَوَى أَبُو هُرَيْرَةَ...",
+		"حَدَّثَنَا مُحَمَّدٌ..."
+	};
 
-    private static readonly string[] Narraters = 
-    {
-        "Abu Huraira",
-        "Ibn Majah",
-        "Al-Bukhari"
-    };
+	private static readonly string[] Narraters =
+	{
+		"Abu Huraira",
+		"Ibn Majah",
+		"Al-Bukhari"
+	};
 
-    [HttpGet]
-    public IActionResult GetHadith()
-    {
-		if (ArabicText == null || ArabicText.Length == 0 
-				|| Narraters == null || Narraters.Length == 0
-				) {
+	[HttpGet]
+	public IActionResult GetHadith()
+	{
+		if (ArabicText == null || ArabicText.Length == 0
+		                       || Narraters == null || Narraters.Length == 0
+		   )
+		{
 			return BadRequest(new { message = "The Arabic text or the narraters are not available." });
 		}
-		
-      try {
-		var hadiths = Enumerable.Range(1, 5).Select(index =>
-				new Hadith
-				(
+
+		try
+		{
+			var hadiths = Enumerable.Range(1, 5).Select(index =>
+					new Hadith
+					(
 						Authentic: Random.Shared.Next(0, 2) == 1,
 						Arabic: ArabicText[Random.Shared.Next(ArabicText.Length)],
 						Narrater: Narraters[Random.Shared.Next(Narraters.Length)]
 					))
 				.ToArray();
-		return Ok(hadiths);
-		} catch (Exception e) {
-				Console.WriteLine(e);
-				return StatusCode(500, new { message = "An error occurred while fetching the hadiths.", detials = e.Message });
+			return Ok(hadiths);
 		}
-    }
+		catch (Exception e)
+		{
+			Console.WriteLine(e);
+			return StatusCode(500, new { message = "An error occurred while fetching the hadiths.", detials = e.Message });
+		}
+	}
 }
--- backend/Controllers/QuranQuotesController.cs
+++ backend/Controllers/QuranQuotesController.cs
@@ -3,53 +3,57 @@ using IlmMachine.Backend.Models;
 
 [Route("api/[controller]")]
 [ApiController]
-public class QuranQuotesController: ControllerBase
+public class QuranQuotesController : ControllerBase
 {
-    private static readonly string[] ArabicText = 
-    {
-        "قُلْ هُوَ اللَّهُ أَحَدٌ",
-        "قُلْ أَعُوذُ بِرَبِّ الْفَلَقِ",
-        "قُلْ أَعُوذُ بِرَبِّ النَّاسِ"
-    };
+	private static readonly string[] ArabicText =
+	{
+		"قُلْ هُوَ اللَّهُ أَحَدٌ",
+		"قُلْ أَعُوذُ بِرَبِّ الْفَلَقِ",
+		"قُلْ أَعُوذُ بِرَبِّ النَّاسِ"
+	};
 
-    private static readonly string[] Narraters = 
-    {
-        "Abu Huraira",
-        "Ibn Majah",
-        "Al-Bukhari"
-    };
+	private static readonly string[] Narraters =
+	{
+		"Abu Huraira",
+		"Ibn Majah",
+		"Al-Bukhari"
+	};
 
-    private static readonly string[] EnglishText = 
-    {
-        "Say, 'He is Allah, [who is] One,'",
-        "Say, 'I seek refuge in the Lord of daybreak'",
-        "Say, 'I seek refuge in the Lord of mankind,'"
-    };
+	private static readonly string[] EnglishText =
+	{
+		"Say, 'He is Allah, [who is] One,'",
+		"Say, 'I seek refuge in the Lord of daybreak'",
+		"Say, 'I seek refuge in the Lord of mankind,'"
+	};
 
-    [HttpGet]
-    public IActionResult GetQuranQuotes()
-    {
-			if (ArabicText == null || ArabicText.Length == 0 
-					|| Narraters == null || Narraters.Length == 0
-					|| EnglishText == null || EnglishText.Length == 0
-					) {
-					return BadRequest(new { message = "The Arabic text, the English text, or the narraters are not available." });
-			}
+	[HttpGet]
+	public IActionResult GetQuranQuotes()
+	{
+		if (ArabicText == null || ArabicText.Length == 0
+		                       || Narraters == null || Narraters.Length == 0
+		                       || EnglishText == null || EnglishText.Length == 0
+		   )
+		{
+			return BadRequest(new { message = "The Arabic text, the English text, or the narraters are not available." });
+		}
 
-		try {
+		try
+		{
 			var quranQuotes = Enumerable.Range(1, 5).Select(index =>
 					new QuranQuotes
 					(
-							Arabic: ArabicText[Random.Shared.Next(ArabicText.Length)],
-							English: EnglishText[Random.Shared.Next(EnglishText.Length)],
-							Narrater: Narraters[Random.Shared.Next(Narraters.Length)]
+						Arabic: ArabicText[Random.Shared.Next(ArabicText.Length)],
+						English: EnglishText[Random.Shared.Next(EnglishText.Length)],
+						Narrater: Narraters[Random.Shared.Next(Narraters.Length)]
 					))
-					.ToArray();
+				.ToArray();
 			return Ok(quranQuotes);
-
-		} catch (Exception e) {
+		}
+		catch (Exception e)
+		{
 			Console.WriteLine(e);
-			return StatusCode(500, new { message = "An error occurred while fetching the Quran quotes.", detials = e.Message });
+			return StatusCode(500,
+				new { message = "An error occurred while fetching the Quran quotes.", detials = e.Message });
 		}
 	}
 }
